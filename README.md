b08775e3d11d8a561b528fddfc5dc095b9f2872e
Mahika Kushwaha <mahikakushwaha@gmail.com>
2021-09-19 18:17:42 +0300

    docs: updated cash register

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a VanillaJS program that calculates the balance amount and also shows the minimum 
+number of notes required to pay the balance found.
---
+++ app.js
@@ -0,0 +1,43 @@
+const billAmount=document.querySelector("#bill-amount");
+const cashGiven=document.querySelector("#cash-given");
+const checkButton=document.querySelector("#btncalc");
+const message=document.querySelector("#error-message");
+
+const availableNotes=[2000,500,100,20,10,5,1];
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+
+checkButton.addEventListener("click", function validateBillCash(){
+
+    message.style.display="none";
+    if(billAmount.value>0)
+    {
+        if(cashGiven.value>=billAmount.value)
+        {
+            const amountReturned=cashGiven.value-billAmount.value;
+            calculateChange(amountReturned);
+        }
+        else 
+        {
+            message.style.display="block";
+            message.innerText="Cash should be Greater or Equal to Bill Amount!"
+        }
+    }
+    else
+    {
+        message.style.display="block";
+        message.innerText="Invalid Bill Amount";
+    }
+})
+
+function calculateChange(amountReturned)
+{
+   for(let i=0;i<availableNotes.length;i++)
+   {
+       const numberofNotes=Math.trunc(amountReturned/availableNotes[i]);
+       amountReturned%=availableNotes[i];
+       noOfNotes[i].innerText=numberofNotes;
+   }
+}
+
+
+
